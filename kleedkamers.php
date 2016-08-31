e3422bc79501967cd05ec085d64e5eddff3965f3
beastdjw <dennis@veldt.it>
2016-08-31 22:57:48 +0200

    lichtkrant in kleedkamers

--- kleedkamers.php
+++ kleedkamers.php
@@ -7,6 +7,7 @@
 <html>
   <head>
         <script type="text/javascript">
+        var messagenr = 1;
         function addZero(i) {
            if (i < 10) {
               i = "0" + i;
@@ -17,6 +18,7 @@
         function timedMsg()
         {
            var t=setInterval("change_time();",1000);
+           var t2=setInterval("changeText();",7000);
         }
         function change_time()
         {
@@ -30,7 +32,29 @@
            document.getElementById('Minut').innerHTML=addZero(curr_min)+':';
            document.getElementById('Second').innerHTML=addZero(curr_sec);
         }
+        function changeText()
+        { 
+           document.getElementById('hide1').style.display = 'none';
+           document.getElementById('hide2').style.display = 'none';
+	   document.getElementById('hide3').style.display = 'none';
+ 
+           switch(messagenr) {
+           	case 1:
+                   document.getElementById('hide1').style.display = 'inline';
+                   messagenr = 2;
+                   break;
+                case 2:
+                   document.getElementById('hide2').style.display = 'inline';
+                   messagenr = 3;
+                   break;
+                case 3:
+                   document.getElementById('hide3').style.display = 'inline';
+                   messagenr = 1;
+                   break;
+           }
+        }   
         timedMsg();   
+        changeText();
     </script>
     <style>
        body {
@@ -65,20 +89,22 @@
        //   font-family: "Lucida Console", Verdana, Arial;
         //  font-size: 1.7em; /* 40px/16=2.5em */
           font-family: Tahoma,Arial Narrow,Arial,sans-serif;
-	font-size: 33px;
+	font-size: 1.7vw;
 	font-style: normal;
 	font-variant: normal;
 	font-weight: 400;
 	line-height: 30.8px;
 
        }
-       th,td {
+       th {
 	  padding: 10px;
           color:#006400;
+          background-color: rgba(102,255,0, 0.9);
        }
        td {
           padding: 10px;
           color:#aaffaa;
+          border-bottom: solid;
        }
 
        .wedstrijden {
@@ -100,15 +126,12 @@
           float:left;
           color: #0;
           font-family: Tahoma,Arial Narrow,Arial,sans-serif;
-          font-size: 22px;
+        font-size: 1.7vw;
         font-style: normal;
         font-variant: normal;
         font-weight: 400;
         line-height: 30px;
 
-       }
-       .tablehead {
-          background-color: rgba(150,230,150, 0.9);
        }
        tr {
           text-align: center;
@@ -156,11 +179,19 @@
    </div>
    
    <div class="fcc-info">
-   <H2>Kleedkamers</H2>
-   <p>Onze vrijwilligers doen hun best om u een nette kleedkamer aan te bieden. Helpt u mee door uw afval in de prullenbak te gooien en de kleedkamer na gebruik even aan te vegen?</p>
+      <div id="hide1">
+         <H2>Kleedkamers</H2>
+         <p>Onze vrijwilligers doen hun best om u een nette kleedkamer aan te bieden. Helpt u mee door uw afval in de prullenbak te gooien en de kleedkamer na gebruik even aan te vegen?</p>
+      </div>
+      <div id="hide2" style="display:none">
+         <H2>Welkom!</H2>
+         <p>Welkom op sportpark Noord End. Op dit scherm vindt u de veld- en kleedkamerindeling voor de wedstrijden van vandaag. Bezoekende clubs, meld u a.u.b. even bij het wedstrijdsecretariaat.</p>
+      </div>
+      <div id="hide3" style="display:none">
+          <H2>Sponsoring</H2>
+         <p>Met reclame maken bij FC Castricum bereikt u niet alleen een zeer grote groep Castricummers, u steunt er ook nog eens uw club mee! Ook sponsor worden bij FC Castricum? Neem contact op met de commissieleden.</p>
+      </div>
    </div>
-
-
 
 
 </body>
